0bb3a5da67b4d1990d79099b09d2171019ec1080
SaikatMandal7672 <saikatmandal290103@gmail.com>
2024-09-15 19:05:04 +0530

    modifications

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import side_eye_cat from "../assets/cat-2.jpeg"
         <div className='flex flex-col items-center justify-center w-full -z-10'>
           <h1 className=' mt-16 overflow-hidden text-[12vw] font-semibold uppercase leading-none'>Saikat <br /> mandal</h1>
           <div className='mt-8 '>
-            <a href="../assets/SAIKAT_MANDAL_JU.pdf"
+            <a href="https://drive.google.com/file/d/1PS43BNGGxcEH4wo_AyNVWASzRFErnPCM/view?usp=drive_link"
             target="_blank" rel='noopener noreferrer '
             download
             className='flex items-center p-2 rounded-xl bg-lime-300 px-3 font-sans font-medium text-black hover:bg-lime-400'
